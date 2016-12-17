35d1ddfb0f38b48cc056c59aadeaabfae9a33ad0
sentosh1ne <fortrust2@gmail.com>
2016-12-17 20:17:39 +0200

    Update NameInputHandler.cs

--- MazeGame/Assets/Scripts/Menu/NameInputHandler.cs
+++ MazeGame/Assets/Scripts/Menu/NameInputHandler.cs
@@ -4,15 +4,17 @@ using UnityEngine.UI;
 using System;
 
 public class NameInputHandler : MonoBehaviour {
-
+	
+    //Reference to input field	
     InputField nameInput;
 
 	void Start ()
-    {
+       {
         nameInput = GetComponent<InputField>();
         nameInput.onValueChanged.AddListener(ChangeName);
 	}
-
+    
+    // changing player preferences	
     private void ChangeName(string arg0)
     {
         PlayerPreferences.playerName = nameInput.text;
